56abfc54775e1c557b910545033aefcf9aafde81
Igor Kislitsyn <k.yngvarr@gmail.com>
2022-09-12 15:13:32 +0700

    update from playrix

---
+++ home_username/.config/nvim/init.lua
@@ -0,0 +1,22 @@
+vim.cmd([[
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vim/vimrc
+
+let $NVIM_TUI_ENABLE_CURSOR_SHAPE = 0
+set guicursor=
+
+set nohlsearch
+]])
+
+-- uncomment for vertical monitor setup
+--require('telescope').setup {
+    --defaults = {
+        --layout_strategy = "vertical",
+        --sorting_strategy = "ascending",
+        --layout_config = {
+            --height = 0.95,
+            --prompt_position = 'top',
+        --},
+    --}
+--}
